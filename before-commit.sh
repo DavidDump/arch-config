a2276749a0e460c9b323e8b143007dc332088b39
David <dbenes10@gmail.com>
2022-06-26 02:34:16 +0200

    fixed trasnparency

--- before-commit.sh
+++ before-commit.sh
@@ -2,3 +2,4 @@ cp ~/.xinitrc ~/arch-config/config/.xinitrc
 cp ~/.config/i3/config ~/arch-config/config/.config/i3/config
 cp ~/.config/fish/config.fish ~/arch-config/config/.config/fish/config.fish
 cp ~/.config/alacritty/alacritty.yml ~/arch-config/config/.config/alacritty/alacritty.yml
+cp ~/.config/compton.conf ~/arch-config/config/.config/compton.conf
---
+++ trasnparency.md
@@ -0,0 +1,14 @@
+# Transparency
+Dependency that I was missing in the VM, full list on repo
+```bash
+sudo pacman -S asciidoc
+```
+
+Install compton
+```bash
+git clone https://github.com/chjj/compton.git
+cd compton
+make
+make docs
+make install
+```
