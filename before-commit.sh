bb443c7170fc1dfbc246e5b9d07ec4ebc19f5fe4
David <dbenes10@gmail.com>
2022-06-27 22:29:16 +0200

    added polybar

--- before-commit.sh
+++ before-commit.sh
@@ -3,3 +3,4 @@ cp ~/.config/i3/config ~/arch-config/config/.config/i3/config
 cp ~/.config/fish/config.fish ~/arch-config/config/.config/fish/config.fish
 cp ~/.config/alacritty/alacritty.yml ~/arch-config/config/.config/alacritty/alacritty.yml
 cp ~/.config/compton.conf ~/arch-config/config/.config/compton.conf
+cp ~/.config/polybar/config.ini ~/arch-config/config/.config/polybar/config.ini
