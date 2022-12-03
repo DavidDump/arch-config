66e477420a0470c236e9c6bfb2b2ff77ac2cbe2e
DavidDump <dbenes10@gmail.com>
2022-12-03 01:46:45 +0100

    fixed copy error

--- before-commit.sh
+++ before-commit.sh
@@ -2,8 +2,8 @@ repopath=~/arch-config
 
 cp ~/.xinitrc $repopath/config/.xinitrc
 cp ~/.config/compton.conf $repopath/config/.config/compton.conf
-cp -r ~/.config/i3/ $repopath/config/.config/i3/
-cp -r ~/.config/fish/ $repopath/config/.config/fish/
-cp -r ~/.config/alacritty/ $repopath/config/.config/alacritty/
-cp -r ~/.config/polybar/ $repopath/config/.config/polybar/
-cp -r ~/scripts/ $repopath/scripts/
+cp -a ~/.config/i3/ $repopath/config/.config/
+cp -a ~/.config/fish/ $repopath/config/.config/
+cp -a ~/.config/alacritty/ $repopath/config/.config/
+cp -a ~/.config/polybar/ $repopath/config/.config/
+cp -a ~/scripts/ $repopath/
